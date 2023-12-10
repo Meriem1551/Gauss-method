601bfd3c85140f0b58cf57c88074e71b9ece5d0e
Meriem1551 <meriem200409@gmail.com>
2023-12-10 18:16:42 +0100

    fixing displayed msg

--- readB.py
+++ readB.py
@@ -3,7 +3,7 @@ import numpy as np
 
 def readB():
     List = input(
-        "Enter a list separeted by comma as B (it must have same number of rows as A)"
+        "Enter a list separeted by comma as B (it must have same number of rows as A) "
     )
     B = [int(i) for i in List.split(",")]
     return B
