811db70137fb297871f0a8879e5dfb33421049d9
unknown <hafsosa10@gmail.com>
2023-12-10 18:45:35 +0100

    gauss-project

---
+++ resolution.py
@@ -0,0 +1,10 @@
+import numpy as np
+
+def resolution(A, B, R):
+    X = np.zeros(R)
+
+    for i in range(R - 1, -1, -1):
+        tmp = np.dot(A[i, i+1:], X[i+1:])
+        X[i] = (B[i] - tmp) / A[i, i]
+
+    return X
